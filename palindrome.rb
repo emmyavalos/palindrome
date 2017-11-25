28bfc20ff87f51ac7f1cedbad9fcea4c322bb92a
Emmy Avalos <emmyavalos415@gmail.com>
2017-11-24 19:50:47 -0700

    palindrom initial

---
+++ palindrome.rb
@@ -0,0 +1,37 @@
+# accept a word or phrase
+# push to array
+# maybe push to 2nd array and loop over array and compare to itself backwoards
+  # length = length - 1?
+
+
+phrase = []
+phrase2 = []
+
+puts "Please enter a phrase into the palindrome checker:"
+phrase << gets.strip.downcase
+
+phrase2 << phrase
+# puts phrase_array
+
+x = phrase.length
+puts x
+
+for x in phrase_array.length
+  while x
+  x -= x-1
+  phrase_rev = x
+end
+end
+@@variable
+$variable
+
+
+# case
+# when #matching condition
+#
+# end
+
+# for x in phrase_array1
+#   phrase_array2 << phrase_array.length[x -=1]
+#   puts phrase_array2
+# end
